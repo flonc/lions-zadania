b252d41af8e8b0c5fcf1a8a0fd6a64ccff0a1b0f
DESKTOP-F5OHIGA\Filip Lonc <filiplonc@gmail.com>
2018-10-21 14:41:25 +0200

    rozwiązanie zadania 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,9 +4,18 @@ public class MainClass {
 	public static void main(String[] args) {
 	
 		MainClass m = new MainClass();
-		String words[] = {"a","bb","b","ccc"};
+		String wordsZad1[] = {"a","bb","b","ccc"};
 		int len = 1;
-		System.out.println(m.wordsCount(words, len));
+		System.out.println("Count of the number of strings with the given length: " + m.wordsCount(wordsZad1, len));
+		
+		String wordsZad2[] = {"a","b","c","d"};
+		int n = 2;
+		if(n <= wordsZad2.length) {
+			m.printNewArray(m.wordsFront(wordsZad2, n));
+		}else {
+			System.out.println("N is bigger than array length!");
+		}
+		
 	}
 	
 	public int wordsCount(String[] words, int len) {
@@ -20,6 +29,27 @@ public class MainClass {
 		
 		return count;
 		
+	}
+	
+	public String[] wordsFront(String[] words, int n) {
+		
+		
+		
+		String newArray[] = new String[n];
+		
+		for(int i=0; i<n; i++) {
+			
+			newArray[i] = words[i];
+		}
+		
+		return newArray;
+		
+	}
+	
+	public void printNewArray(String[] array) {
+		System.out.println("New array containing the first N strings: ");
+		for(int i=0; i<array.length; i++)
+			System.out.println(array[i]);
 	}
 
 }
