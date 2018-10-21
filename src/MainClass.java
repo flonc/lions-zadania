4971e8003c907e891897efd87f4e5a2325faeb23
DESKTOP-F5OHIGA\Filip Lonc <filiplonc@gmail.com>
2018-10-21 14:01:00 +0200

    rozwiązanie zadania nr1 wordsCount

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,6 +3,22 @@ public class MainClass {
 
 	public static void main(String[] args) {
 	
+		MainClass m = new MainClass();
+		String words[] = {"a","bb","b","ccc"};
+		int len = 1;
+		System.out.println(m.wordsCount(words, len));
+	}
+	
+	public int wordsCount(String[] words, int len) {
+		
+		int count = 0;
+		for (int i=0; i<words.length; i++) {
+			if (words[i].length() == len) {
+				count++;
+			}
+		}
+		
+		return count;
 		
 	}
 
